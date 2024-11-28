2bfcfb5ee620b9a13dabf1e6f0da6a576caecb39
Timmy <adistats99@gmail.com>
2024-11-28 09:56:27 +0500

    my first commit

---
+++ cal.js
@@ -0,0 +1,8 @@
+function putonStack() {
+  // e.preventDefault();
+  // const ans = document.getElementById("button-8").innerText;
+  // const ans = document.getElementById("button-8").getAttribute("d-value");
+
+  console.log(data.getAttribute("d-value"));
+  console.log(data.innerText);
+}
